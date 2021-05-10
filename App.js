cb13196c6ea54e84841007004cd889a6beab5029
Mammadali Gasimov <mammadalig@wizardsdev.com>
2021-05-10 08:24:40 +0400

    first commit, setting up project

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = () => (
+  <View style={{justifyContent: 'center', alignItems: 'center', backgroundColor: 'skyblue', flex: 1}}>
+    <Text>STAR WARS APP</Text>
+    <Text>From Star Wars fan to Star Wars fans;)</Text>
+  </View>
+);
+
+export default App;
